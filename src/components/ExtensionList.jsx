74ea0ca4375ef70f288b196e5e9f101e8bede787
GeorgeKandelaki <kandelakig29@hotmail.com>
2025-11-02 15:55:07 +0400

    adding

--- src/components/ExtensionList.jsx
+++ src/components/ExtensionList.jsx
@@ -4,9 +4,7 @@ import ExtensionItem from "./ExtensionItem";
 const List = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
     justify-content: center;
-    justify-items: center;
     gap: 1.3rem;
 
     @media screen and (max-width: 65em) {
